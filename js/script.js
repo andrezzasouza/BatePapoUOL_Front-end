2e3c170448cdccc080e80465c66d8a8dc6fc0306
Andrezza Souza <andrezzasouza@id.uff.br>
2021-08-07 14:20:43 -0300

    Carregou os 3 tipos mensagens como no layout

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const POST_USER_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants";
-const GET_MESSAGES_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages"
+const GET_MESSAGES_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages";
 
 let yourName;
 let sendContent;
 let messageStatus;
+let username = {};
 
 checkUsername("Qual é seu lindo nome?");
 
@@ -12,9 +13,10 @@ function checkUsername (pergunta) {
     do {
         yourName = prompt(pergunta);
     } while(yourName === "");
+    // Ainda deixa o nome ir vazio quando clica em "Cancelar" o prompt
     
 
-    let username = {
+    username = {
         name: `${yourName}`
     }
 
@@ -47,7 +49,6 @@ function postAnswer(postResponse) {
             }
         } while (postResponse.status !== 200)
     }
-    
     console.log("Chegou até aqui");
     console.log(postResponse);
 }
@@ -58,14 +59,48 @@ function ifSuccessful (respose) {
 
     getContent.then(loadMessages);
     getContent.catch(console.log("Deu ruim"));
-
 }
 
 function loadMessages(messageResult) {
     const messages = messageResult.data;
-    console.log(messages);
+    console.log("data", messages);
 
     const messageContainer = document.querySelector("main");
     let innerContainer = "";
+
+    for (let i = 0; i < messages.length; i++) {
+        if (messages[i].type === "status") {
+            innerContainer += `<div class="message ${messages[i].type}">
+            <span class="timestamp">(${messages[i].time})</span>
+            <span class="actor">${messages[i].from}</span>
+            <span class="sent-message">${messages[i].text}</span>
+         </div>`
+        } else if (messages[i].type === "message") {
+            innerContainer += `<div class="message">
+            <span class="timestamp">(${messages[i].time})</span>
+            <span class="actor">${messages[i].from}</span>
+            <span class="action">para</span>
+            <span class="receiver">${messages[i].to}:</span>
+            <span class="sent-message">${messages[i].text}</span>
+         </div>`
+        } else if (messages[i].type === "private_message") {
+            if (username === messages[i].from || username === messages[i].to)
+            innerContainer += `<div class="message ${messages[i].type}">
+            <span class="timestamp">(${messages[i].time})</span>
+            <span class="actor">${messages[i].from}</span>
+            <span class="action">reservadamente para</span>
+            <span class="receiver">${messages[i].to}:</span>
+            <span class="sent-message">${messages[i].text}</span>
+         </div>`
+        }
+    }
+
+    messageContainer.innerHTML = innerContainer;
 }
+
+// function userStatus() {
+//     axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/status", username);
+// }
+
+// setInterval(userStatus, 5000);
 
