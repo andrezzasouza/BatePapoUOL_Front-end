dd0fdec030248af5b61c8481b57581c0633ee076
Andrezza Souza <andrezzasouza@id.uff.br>
2021-08-07 12:42:00 -0300

    Configurou validação do nome do usuário

--- js/script.js
+++ js/script.js
@@ -1,15 +1,71 @@
-// Usar funções para verificar o nome ou autenticar entrada?
+const POST_USER_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants";
+const GET_MESSAGES_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages"
 
-// Pegar nome de usuário com querySelector + value se for num input ou de outra forma de for do prompt
+let yourName;
+let sendContent;
+let messageStatus;
 
-const yourName = prompt("Qual é seu lindo nome?");
+checkUsername("Qual é seu lindo nome?");
 
-let username = {
-    name: `${yourName}`
+function checkUsername (pergunta) {
+
+    do {
+        yourName = prompt(pergunta);
+    } while(yourName === "");
+    
+
+    let username = {
+        name: `${yourName}`
+    }
+
+    sendContent = axios.post(POST_USER_URL, username);
+
+    console.log(sendContent.then());
+    console.log(sendContent.catch());
+
+    sendContent.then(postAnswer);
+    sendContent.catch(postAnswer);
+
+    console.log("PA")
+    console.log(postAnswer);
 }
 
-const sendContent = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants", username);
+function postAnswer(postResponse) {
+
+    messageStatus = postResponse;
+    console.log("msg", messageStatus)
+
+    if (postResponse.status === 200) {
+        ifSuccessful(postResponse);
+    } else {
+        do {
+            if (postResponse.response.status === 400) {
+                checkUsername("O nome escolhido já está em uso. Escolha outro nome, por favor.");
+                return checkUsername;
+            } else {
+                alert("Algo deu errado, mas não sei o que. Tente novamente.");
+            }
+        } while (postResponse.status !== 200)
+    }
+    
+    console.log("Chegou até aqui");
+    console.log(postResponse);
+}
 
 
-sendContent.then(ifSuccessful); // Preencher com a função
-sendContent.catch(ifError); // Preencher com a função
+function ifSuccessful (respose) {
+    const getContent = axios.get(GET_MESSAGES_URL);
+
+    getContent.then(loadMessages);
+    getContent.catch(console.log("Deu ruim"));
+
+}
+
+function loadMessages(messageResult) {
+    const messages = messageResult.data;
+    console.log(messages);
+
+    const messageContainer = document.querySelector("main");
+    let innerContainer = "";
+}
+
